d826492fefb129023430a61ba389d9e724c7731a
Kamalesh Suthar <kamaleshsuthar@Kamaleshs-MacBook-Air.local>
2021-02-09 12:01:56 +0530

    container name changes

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import classes from './About.module.css'
 
-class About extends React.Component{
-    render(){
-        return(
-            <div className={classes.AboutMainDiv}>
-                <p>
-                    Hello <span className={classes.BloggerText}>Bloggers</span>, Welcome to <b>BlogSport</b>.<br></br> 
-                    Here is your dream come true moment, where 
-                    you can create your own website and blog as 
-                    per your convinience
-                </p>               
+const About = () => {
+
+    return(
+        <div className={classes.MainContainer}>
+            <div className={classes.LeftSection}>
+dssd
             </div>
-        );
-    }
+            <div className={classes.RightSection}>
+dscsd
+            </div>
+        </div>
+    )
 }
+
 
 export default About

1a904ffbfda2661681c4a4f3b90fcdb1528ced7e
Kamalesh Suthar <kamaleshsuthar@Kamaleshs-MacBook-Air.local>
2021-02-09 11:58:44 +0530

    kamalesh

---
+++ src/Containers/Home-Landing/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './Home.module.css'
+
+const Home = () => {
+
+    return(
+        <div className={classes.MainContainer}>
+            <div className={classes.LeftSection}>
+dssd
+            </div>
+            <div className={classes.RightSection}>
+dscsd
+            </div>
+        </div>
+    )
+}
+
+
+export default Home

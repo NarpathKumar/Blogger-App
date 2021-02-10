f5915fb61ab973801a317b143d4dc0db515b3f16
Kamalesh Suthar <kamaleshsuthar@Kamaleshs-MacBook-Air.local>
2021-02-10 12:35:48 +0530

    minor bug fixes

--- src/Containers/Templates/Template.js
+++ src/Containers/Templates/Template.js
@@ -4,7 +4,17 @@ import classes from './Template.module.css'
 class Template extends React.Component{
     render(){
         return(
-            <div></div>
+            <div className={classes.MainDiv}>
+                <div className={classes.SelectionDiv}>
+                    <div className={classes.SubSelectDiv}>
+                        <p>If you want to select system created designs for your websites you can click here</p>
+                        <button>Create App using system Designs</button>
+                    </div>
+                    <div className={classes.SubSelectDiv}>
+
+                    </div>
+                </div>
+            </div>
         );
     }
 }
